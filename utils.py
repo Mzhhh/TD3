5a510e12270275a8de5e2fe146e874da14f1a60e
Zihan Mao <46877599+Mzhhh@users.noreply.github.com>
2021-05-26 14:05:17 +0800

    fuck buffer

--- utils.py
+++ utils.py
@@ -10,12 +10,12 @@ class ReplayBuffer(object):
 		self.ptr = 0
 		self.size = 0
 
-		self.state = np.zeros((max_size, state_dim))
+		self.state = np.zeros((max_size, *state_dim))
 		self.action = np.zeros((max_size, action_dim))
-		self.next_state = np.zeros((max_size, state_dim))
+		self.next_state = np.zeros((max_size, *state_dim))
 		self.reward = np.zeros((max_size, 1))
 		self.not_done = np.zeros((max_size, 1))
-
+		
 		self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 	def add(self, state, action, next_state, reward, done):
